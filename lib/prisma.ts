73643918febe9eb0c7839ba73ab54b25b31f671b
Smart-Rwl <110030446+Smart-rwl@users.noreply.github.com>
2025-12-10 07:52:20 +0000

    Remove .env from repository

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,13 +6,7 @@ const globalForPrisma = global as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  // Prisma 7 requires explicit options; types may lag, so we ignore TS here.
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL!,
-  });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
